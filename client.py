57181da826f6cdf5c41df375d893b811b4d651b2
adjhawar <adjhawar0697@gmail.com>
2018-11-15 18:44:45 +0530

    Whatever

--- client.py
+++ client.py
@@ -15,17 +15,21 @@ ip_address=sys.argv[1]
 port=int(sys.argv[2])
 s.connect((ip_address,port))
 
-flag=0
-while flag>=0:
-	try:
-		data=s.recv(1024).decode('utf-8')
-		if data:
-			print(data)
-		print("Enter the data")	
-		data=input()
-		s.send(data.encode('utf-8'))
-	except:
-		continue
+msg=s.recv(1024).decode('utf-8')
+print(msg)
+data=input()
+s.send(data.encode('utf-8'))
+'''msg=s.recv(1024).decode('utf-8')
+if num!=1 and num!=2:
+	s.close()
+elif int(data)==2:
+	name=input()
+else:
+	name=""
+user=input()
+password=input()
+to_send=name+"\n"+user+"\n"+password
+s.send(to_send.encode('utf-8'))
+msg=s.recv(1024).decode('utf-8')'''
 s.close()
-print(data)
 
--- server.py
+++ server.py
@@ -1,46 +1,140 @@
 import socket
 import os
 import sys
+import queue
+from threading import Thread
 from _thread import *
+import pandas as pd
+import numpy as np
+
+#1.Login
+#2.Register
+#3.Send
+#4.Broadcast to group of users
+#5.Broadcast to all users
+#0.Logout 
 
 BUF_SIZE=1024
 active_list=[]
-
+queues={}
+user_fd={}
+threads=[]
 #authenticates the username and password
-def login(username,password):
-	return b'1'
+def login(user,password):
+	df=pd.read_csv("user.csv")
+	i=df.loc[df['Username'] == user]
+	if df['Password'][i]==password:
+		return 1
+	elif user in active_user:
+		return 0
+	else:
+		return 0
 
 #register a new user
-def register(s):
-	return b'1'
+def register(name,user,password):
+	df=pd.read_csv("user.csv")
+	if df['Username']==user:
+		return 0
+	else:
+		df.loc[-1]=[name,user,password]
+		df.index=df.index+1
+		df.to_csv("user.csv",index=False)
+		return 1
 
 def remove(c):
 	if c in active_list:
 		active_list.remove(c)
-
-def clientThread(c,addr):
-	c.send(b"Welcome to the Server")
-	flag=0
-	while flag>=0:
-		try:
-			msg=c.recv(BUF_SIZE)
-			if msg:
-				print(msg)
-			else:
-				remove(c)
-		except:
-			continue
-
+       
+class clientReceive(Thread):
+	def __init__(self,socket,ip,port):
+		Thread.__init__(self)
+		self.socket=socket
+		self.ip=ip
+		self.port=port
+	
+	def run(self):
+		s=self.socket
+		fd=s.fileno()
+		s.send(b"Welcome to the Server\n1.Login\n2.Register\nAny other key to Logout")
+		username=""
+		result=0
+		while True:
+			try:
+				msg=s.recv(BUF_SIZE).decode('utf-8')
+				words=msg.split(" ")
+				cmd=words[0]
+				if cmd=="login" and result==0:
+					username=words[1]
+					result=login(words[1],words[2])
+					if result:
+						s.send(b"Login successful")
+						active_user.append(username)
+						#add mapping from username to fd
+				elif cmd=="register" and result==0:
+					username=words[2]
+					result=register(words[1],words[2],words[3])
+					if result:
+						s.send(b"Registration successful")
+						active_user.append(username)
+						#add mapping from username to fd
+				elif result==1:
+					if cmd=="online":
+						msg=""
+						for p in active_user:
+							if p!=username:
+								p=msg+" "+p
+						s.send(msg.encode('utf-8')
+					elif cmd=="send":
+						receiver=words[1]
+						msg=username+">>"
+						for i in words[2:]:
+							msg=msg+" "+i
+					elif cmd=="broadcast":
+						msg=username+">>"
+							for i in words[2:]:
+								msg=msg+" "+i
+						if words[1]=="all":
+							for p in active_user:
+								if p!=username:
+									#add to the respective users' shared queue
+									print(p)
+						else:
+							receivers=words[1].split(",")
+							for receiver in receivers:
+								if receiver in active_user:
+									#add to the respective users shared queue
+									print("active",receiver)
+								else:
+									#write to his respective file
+									print("inactive",receiver)
+					elif cmd=="logout":
+						#remove the user
+						print("logout",username)
+					elif cmd=="inbox":
+						print("Inbox requested")
+					else:
+						s.send(b"Enter a valid command")
+				else:
+					sys.exit()					
+			except:
+				continue
+				
 #AF_INET implies IPv4 and SOCK_STREAM implies TCP connection
 port=int(sys.argv[1])
+#creates the first socket where server only receives
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#binding the server to a port
 s.bind(('127.0.0.1',port))
 s.listen()
 print("Server is listening")
 while True:
 	c,addr=s.accept()
-	active_list.append(c)
-	start_new_thread(clientThread,(c,addr))
+	q=queue.Queue()
+	queues[c.fileno()]=q
+	thread1=clientReceive(c,'127.0.0.1',port)
+	thread1.daemon=True
+	thread1.start()
+	threads.append(thread1)
+
+for t in threads:
+	t.join()
 s.close()
